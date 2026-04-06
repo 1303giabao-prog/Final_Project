63dc40e56e7a6583952b810ecdde1aa415f9f0aa
Bao Nguyen <ngiabao624@gmail.com>
2026-04-06 16:01:59 -0600

    Adding Attributes, Constructor, Getters and Setters, toString (Second Commit-Bao)

--- Final_Project/src/Customer.java
+++ Final_Project/src/Customer.java
@@ -1,5 +1,70 @@
-
 public class Customer {
-	private String email;
 
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNum;
+    private String membership;
+
+    // Constructor
+    public Customer(String email, String firstName, String lastName, String phoneNum, String membership) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.membership = membership;
+    }
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getMembership() {
+        return membership;
+    }
+
+    public void setMembership(String membership) {
+        this.membership = membership;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                ", membership='" + membership + '\'' +
+                '}';
+    }
 }
---
+++ Final_Project/src/GymManagmentSystem.java
@@ -0,0 +1,51 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class GymManagmentSystem {
+	
+	 // Global connection variable
+    static Connection conn = null;
+    
+    // Database credentials.
+    private static final String SERVER = "localhost";
+    private static final int PORT = 3306;
+    private static final String DATABASE = "demo211";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "P@ssw0rd"; 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   connect();      // Connect to DB
+	       disconnect(); 
+	}
+
+
+	
+	//Connect to database
+    public static void connect() {
+        final String DB_URL = String.format("jdbc:mariadb://%s:%d/%s?user=%s&password=%s", 
+                                            SERVER, PORT, DATABASE, USERNAME, PASSWORD);
+        try {
+            conn = DriverManager.getConnection(DB_URL);
+            System.out.println("DB connection established.");
+        } catch(SQLException e) {
+            System.out.println("Problem with connecting to DB: " + e.getMessage());
+        }
+    }
+
+    // Safely close the database connection
+    public static void disconnect() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("DB connection safely closed.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Problem closing the DB connection: " + e.getMessage());
+        }
+    }
+    
+	
+	
+
+}
